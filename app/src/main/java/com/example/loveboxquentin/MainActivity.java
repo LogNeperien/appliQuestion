6e59bd3b9919dc81fe7f8d407efaff63fe32a331
unknown <helene.carlier.gubler@gmail.com>
2021-02-04 18:36:29 +0100

    lien première et deuxieme acitivité

--- app/src/main/java/com/example/loveboxquentin/MainActivity.java
+++ app/src/main/java/com/example/loveboxquentin/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.loveboxquentin;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.View;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonQuestion = findViewById(R.id.buttonQuestion);
+    }
+
+
+    public void launchSecondActivity(View view) {
+        switch (view.getId()) {
+
+            case R.id.buttonQuestion:
+                Intent i = new Intent(this, Question.class);
+                startActivity(i);
+                break;
+
+            default:
+                //Envoyer l'id du match dans un extra en fonction du bouton cliqué
+        }
     }
 }
